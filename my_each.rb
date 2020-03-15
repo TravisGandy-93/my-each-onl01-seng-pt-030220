0710393cf4df794c620ca5b051d8a01cdfa8773e
Travis Gandy <travis_gandy@yahoo.com>
2020-03-15 06:43:52 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+collection = [1, 2, 3, 4]
+
+def my_each(collection)
+  i = 0 
+  while collection.size < 10
+  yield collection[i]
+  i = i + 1
+  end
+end
+
+my_each([1, 2, 3, 4]) do |n|
+  puts n 
 end
